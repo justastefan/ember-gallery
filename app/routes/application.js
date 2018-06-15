9fd586c21b0842b80f39038154545a03e070fdcb
Stefan Riehmer <>
2018-06-15 22:38:30 +0200

    foo

---
+++ app/routes/application.js
@@ -0,0 +1,66 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    model() {
+        return {
+            "photos": [
+                {
+                    "src": "images/wanderung/180510_mday_2018-5216.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5219.jpg",
+                    "ratio": 0.666667
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5221.jpg",
+                    "ratio": 0.666667
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5222.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5223.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5226.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5228.jpg",
+                    "ratio": 0.666667
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5230.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5232.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5238.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5248.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5258.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5263.jpg",
+                    "ratio": 1.5
+                },
+                {
+                    "src": "images/wanderung/180510_mday_2018-5266.jpg",
+                    "ratio": 0.666667
+                }
+            ]
+        };
+    }
+});
